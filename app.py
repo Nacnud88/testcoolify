a760096e1a022a8a5402c056541f0fc96d4cfd52
Nacnud88 <106039226+Nacnud88@users.noreply.github.com>
2025-09-30 18:16:04 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "change-me"  # or read from env
+
+    @app.get("/")
+    def index():
+        return "Flask is alive!"
+
+    @app.get("/api/health")
+    def health():
+        return jsonify(status="ok")
+
+    return app
